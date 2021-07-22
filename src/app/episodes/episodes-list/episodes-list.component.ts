14d5e9764f3dfbeed75d7756e2d9d80ad8263fd4
andreapdev <andreap.dev@gmail.com>
2021-07-22 12:18:42 +0200

    main components setup, layout setup

---
+++ src/app/episodes/episodes-list/episodes-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-episodes-list',
+  templateUrl: './episodes-list.component.html'
+})
+export class EpisodesListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/episodes/episodes.module.ts
+++ src/app/episodes/episodes.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { EpisodesComponent } from './episodes.component';
+import { EpisodesListComponent } from './episodes-list/episodes-list.component';
 
 @NgModule({
-  declarations: [EpisodesComponent],
+  declarations: [EpisodesComponent, EpisodesListComponent],
   exports: [EpisodesComponent]
 })
 export class EpisodesModule {}
